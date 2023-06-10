5a2fc8bc7863987fc2aa802194a6f669a62177ce
Liron Shish <lironshish1@gmail.com>
2023-06-10 14:02:12 +0300

    Login View

---
+++ To Do List/Views/HeaderView.swift
@@ -0,0 +1,40 @@
+//
+//  HeaderView.swift
+//  To Do List
+//
+//  Created by Student21 on 10/06/2023.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 0)
+                .foregroundColor(Color.pink)
+                .rotationEffect(Angle(degrees: 15))
+            
+            VStack{
+                Text("To Do List")
+                    .font(.system(size: 50))
+                    .foregroundColor(Color.white)
+                    .bold()
+                
+                Text("Get thing done")
+                    .font(.system(size: 30))
+                    .foregroundColor(Color.white)
+            }
+            .padding(.top, 30)
+        }
+        .frame(width: UIScreen.main.bounds.width * 3,
+               height: 300)
+        .offset(y: -100)
+
+    }
+}
+
+struct HeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeaderView()
+    }
+}
