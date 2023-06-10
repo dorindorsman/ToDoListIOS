39f5cbfa8b519f217c611993d6db65f9f8cc1d84
Liron Shish <lironshish1@gmail.com>
2023-06-10 14:50:05 +0300

    Register View

--- To Do List/ViewModels/LoginViewViewModel.swift
+++ To Do List/ViewModels/LoginViewViewModel.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+class LoginViewViewModel: ObservableObject{
+    
+    @Published var email = ""
+    @Published var password = ""
+    
+    init(){
+        
+    }
+    
+    func login(){
+        
+    }
+    
+    func validate(){
+        
+    }
+}
--- To Do List/Views/RegisterView.swift
+++ To Do List/Views/RegisterView.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct RegisterView: View {
+    
+    @State var email = ""
+    @State var name = ""
+    @State var password = ""
+    
     var body: some View {
         
         VStack {
@@ -16,6 +21,31 @@ struct RegisterView: View {
                        subtitle: "Start organizing todos",
                        angle: -15,
                        background: .orange)
+            
+            Form {
+                TextField("Full Name", text: $name)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                    .autocorrectionDisabled()
+
+                
+                TextField("Email Address", text: $email)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                    .autocapitalization(.none)
+                    .autocorrectionDisabled()
+                
+                SecureField("Password", text: $password)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                
+                TLButton(
+                    title: "Create Account",
+                    background: .green
+                ) {
+                    // Attempt registeration
+                }
+                .padding()
+            }
+            .offset(y: -50)
+            
             Spacer()
         }
     }
