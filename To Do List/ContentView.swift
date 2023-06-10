c4cfbf6ac44b42823c4381a873a2e191df938817
Dorin Dorsman <dorsmandorin@gmail.com>
2023-06-10 11:28:20 +0300

    Initial Commit

---
+++ To Do List/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  To Do List
+//
+//  Created by Student18 on 10/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ To Do List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To Do List
+//
+//  Created by Student18 on 10/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
